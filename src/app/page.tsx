7b9cd9e4888c76b9f49eb5c1174713b8502b163e
Vitor Sousa Dias <105614328+victorrdias@users.noreply.github.com>
2024-07-31 20:45:27 -0300

    feat: create call to action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { CallToAction } from "@/components/callToAction/CallToAction";
 import { Slider } from "@/components/slider";
 
 import "swiper/css";
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0 md:p-24">
       <Slider />
+      <CallToAction />
     </main>
   );
 }
---
+++ src/components/callToAction/SwiperSection.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+import Image, { StaticImageData } from "next/image";
+import { useRef } from "react";
+import "swiper/css/effect-cards";
+import "swiper/css/effect-coverflow";
+import "swiper/css/effect-fade";
+import "swiper/css/grid";
+import "swiper/css";
+
+import image from "../../../assets/images/murangu.webp";
+
+const images: StaticImageData[] = Array(11).fill(image);
+
+export const SwiperSection = ({
+  effect,
+  speed,
+  autoplay,
+}: {
+  effect: any;
+  speed: number;
+  autoplay: any;
+}) => (
+  <div
+    className="relative h-full w-full"
+    style={{ transform: "rotate(-30deg)" }}
+  >
+    <Swiper
+      ref={useRef(null)}
+      normalizeSlideIndex={true}
+      resistanceRatio={0}
+      speed={speed}
+      autoplay={autoplay}
+      modules={[effect]}
+      direction="vertical"
+      slidesPerView={"auto"}
+      resistance={false}
+      loop={true}
+      className="swiper_container flex max-h-[500px]"
+    >
+      {images.map((img, index) => (
+        <SwiperSlide
+          key={index}
+          className="z-10 overflow-hidden rounded-xl p-4"
+        >
+          <Image
+            src={img}
+            alt="nft"
+            width={200}
+            height={250}
+            className="rounded-lg object-cover"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  </div>
+);
