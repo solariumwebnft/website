7081fd46f6acb996c8ce8516e9c7a617bcd1e183
Pedro Ferrari <pedrosantanaferrari96@gmail.com>
2024-08-12 12:19:15 -0300

    [SO-17] Create sitemap (#22)
    * feat: install babel and initialize sitemap
    
    * feat: add sitemap package
    
    * trying to fix building error
    
    * feat: adjust sitemap file structure
    
    * adjust todo list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,6 +43,13 @@ const Scene = dynamic(() => import("@/components/3Dmodels/Scene"), {
  * @todo [done] fix the width of call to action component (PAY ATTENTION TO THE RESPONSIVE)
  * @todo [] add background to sessions
  * @todo SEO
+ * @todo icone da tab do navegador
+ * @todo metadados
+ * @todo [done] titulo do site
+ * @todo descricao do site
+ * @todo [done] site map
+ * @todo robots.txt
+ * @todo open graph
  */
 
 export default function Home() {
