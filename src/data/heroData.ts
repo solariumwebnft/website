3163b8ed31081aeed9bfd0a497f4a346c0aa6ff4
Pedro Ferrari <pedrosantanaferrari96@gmail.com>
2024-08-15 16:20:22 -0300

    refac: adjust video from hero (#40)

--- src/data/heroData.ts
+++ src/data/heroData.ts
@@ -4,7 +4,7 @@ export const HeroContent = () => {
   const subTitle: string =
     " Buy and sell NFTs from the world’s top artists. More than 1,000 premium digital artworks are available to be yours!";
 
-  const src: string = "https://www.w3schools.com/html/mov_bbb.mp4";
+  const src: string = "/solarium-hero-video.mp4";
 
   return { title, subTitle, src };
 };
