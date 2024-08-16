4b0f7186942f4357f533eb85b299dda916f12cca
pedr0ferrari <pedrosantanaferrari96@gmail.com>
2024-08-16 13:59:43 -0300

    feat: add some textual contents to project

--- src/data/heroData.ts
+++ src/data/heroData.ts
@@ -1,8 +1,8 @@
 export const HeroContent = () => {
-  const title: string = "Create, Explore & Collect Digital Art NFTs";
+  const title: string = "Explore, Discover& Conquer Solarium";
 
   const subTitle: string =
-    " Buy and sell NFTs from the world’s top artists. More than 1,000 premium digital artworks are available to be yours!";
+    "An immersive and strategic NFT game that combines resource mining, space exploration and cryptocurrency trading.";
 
   const src: string = "/solarium-hero-video.mp4";
 
