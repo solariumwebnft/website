defee2bd8e2a63909c51aa42c872fb0d04d0b304
pedr0ferrari <pedrosantanaferrari96@gmail.com>
2024-08-19 18:41:26 -0300

    adjust disclaimer and terms of service pages

--- src/app/disclaimer/page.tsx
+++ src/app/disclaimer/page.tsx
@@ -1,11 +1,10 @@
 import { Footer } from "@/components/footer/footer";
 import { Navbar } from "@/components/navbar/navbar";
 import { SparklesBackground } from "@/components/sparklesBackground";
-import { TermsOfServiceData } from "@/data/TermsOfServiceData";
-
+import { DisclaimerData } from "@/data/DisclaimerData";
 import React from "react";
 
-const TermsOfServicePage = () => {
+const Disclaimer = () => {
   return (
     <div className="relative z-[0] m-0 flex min-h-screen w-full flex-col bg-background">
       <SparklesBackground />
@@ -15,9 +14,9 @@ const TermsOfServicePage = () => {
       <main className="flex flex-grow flex-col justify-center px-10 pt-[4rem] md:px-20">
         <div className="flex max-w-full flex-col gap-4 py-20 text-center text-white xl:px-36">
           <h1 className="pb-10 text-center text-[40px] font-extrabold md:text-left lg:text-[45px] xl:text-center xl:text-[64px]">
-            Terms of Service
+            Disclaimer
           </h1>
-          <TermsOfServiceData />
+          <DisclaimerData />
         </div>
       </main>
       <footer className="mt-auto w-full">
@@ -27,4 +26,4 @@ const TermsOfServicePage = () => {
   );
 };
 
-export default TermsOfServicePage;
+export default Disclaimer;
---
+++ src/data/DisclaimerData.tsx
@@ -0,0 +1,29 @@
+export const DisclaimerData = () => {
+  return (
+    <div className="max-w-[1382px] space-y-5 text-left font-barlow text-p16">
+      <p>
+        Please read this "Disclaimer" section carefully. We recommend that you
+        consult your own legal, financial, tax or other professional advisors
+        before engaging in any activity related to Solarium. Nor any service
+        provider shall be liable for any type of direct or indirect damage or
+        loss you may suffer by accessing this document, the whitepaper, the
+        website at solarium.io or any other website or materials published by
+        the company. Any investment in any product or service related to
+        solarium.io, is your responsibility.
+      </p>
+      <p>
+        Based on this information, we invite you to get to know the project and
+        if it makes sense for your portfolio, we welcome you. The products and
+        services sold on this site should not be interpreted as a promise or
+        guarantee of results. Your level of success in achieving the results
+        advertised through the use of our products and information depends on
+        the time you devote to applying the method, techniques used, knowledge
+        and different skills. As these factors differ for each individual, we
+        cannot guarantee success or the level of results, nor are we responsible
+        for any of your actions. Any and all forward-looking statements
+        contained on this site or in any of our products are intended to express
+        our opinion about the potential results that some people may achieve.
+      </p>
+    </div>
+  );
+};
